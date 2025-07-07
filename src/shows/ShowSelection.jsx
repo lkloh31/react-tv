5a044c502ccd5ec8e32f85980dbcff2f4d4a66e9
Lokkee Loh <lkloh31@gmail.com>
2025-07-07 11:51:13 -0700

    final commit

--- src/shows/ShowSelection.jsx
+++ src/shows/ShowSelection.jsx
@@ -1,6 +1,25 @@
 import "./shows.css";
+import { useState } from "react";
 
 /** A navbar that allows users to choose between a list of shows */
-export default function ShowSelection() {
-  return <nav className="shows"></nav>;
+export default function ShowSelection({ shows, setSelectedShow }) {
+  const [selectedShowName, setSelectedShowName] = useState();
+  const handleShowClick = (show) => {
+    setSelectedShow(show);
+    setSelectedShowName(show.name);
+  };
+
+  return (
+    <nav className="shows">
+      {shows.map((show) => (
+        <a
+          key={show.name}
+          onClick={() => handleShowClick(show)}
+          className={selectedShowName === show.name ? "show selected" : "show"}
+        >
+          {show.name}
+        </a>
+      ))}
+    </nav>
+  );
 }
